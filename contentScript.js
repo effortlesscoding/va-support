5303461d21d09cffa918bf6bf2f621308064b0bc
Mike Dao <theeffortlesscoding@gmail.com>
2024-11-02 19:54:05 +0400

    NOISSUE: Updated the extension.

---
+++ contentScript.js
@@ -0,0 +1,15 @@
+function updateFlatmates() {
+    const css = `
+        #nav-myaccount {
+            display: none;
+        }
+    `;
+    const style = document.createElement('style');
+    const head = document.head || document.getElementsByTagName('head')[0];
+    style.type = 'text/css';
+    style.appendChild(document.createTextNode(css));
+    head.appendChild(style);
+
+    document.querySelector('#nav-myaccount').remove();
+}
+updateFlatmates();
